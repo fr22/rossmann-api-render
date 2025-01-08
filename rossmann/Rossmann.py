7822393aa28039672adc322b4994f1cad52afcd7
Francisco Avelar <fr_avelarsilva@yahoo.com>
2025-01-08 15:31:00 -0300

    Changed home_path

--- rossmann/Rossmann.py
+++ rossmann/Rossmann.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 import math
 import datetime
+import os
 
 class Rossmann(object):
     def __init__(self):
-        self.home_path = ''
+        self.home_path = os.path.dirname(os.path.abspath(__file__))
         self.competition_distance_scaler = pickle.load( open(self.home_path + '/parameter/competition_distance_scaler.pkl', 'rb'))
         self.promo_time_week_scaler = pickle.load( open(self.home_path + '/parameter/promo_time_week_scaler.pkl', 'rb'))
         self.competition_time_month_scaler = pickle.load( open(self.home_path + '/parameter/competition_time_month_scaler.pkl', 'rb'))
